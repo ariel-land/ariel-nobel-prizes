3d48b0c753621c3f162a42c4813595f3160ea7f1
Ariel Lee <arielslove629@gmail.com>
2021-11-12 01:08:49 +0900

    Cleaning up JS and html

--- script.js
+++ script.js
@@ -46,27 +46,3 @@ function handleSubmit(event) {
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
-
-// function getCategories() {
-//   // for loop over nobels.prizes
-//   for (const object of nobels.prizes) {
-//     for (const name of object.laureates) {
-//       let headerTwo = document.createElement("h2");
-//       headerTwo.textContent = `Award Year: ${object.year} | Category: ${object.category}`;
-//       headerTwo.style.textTransform = "capitalize";
-//       headerTwo.style.paddingLeft = "20px";
-//       headerTwo.style.fontSize = "25px";
-//       let nameList = document.createElement("p");
-//       nameList.textContent = `Name: ${name.firstname} ${name.surname}`;
-//       nameList.style.paddingLeft = "20px";
-//       nameList.style.fontSize = "18px";
-//       let motivation = document.createElement("p");
-//       motivation.textContent = `Motivation: ${name.motivation}.`;
-//       motivation.style.paddingLeft = "20px";
-
-//       document.body.append(headerTwo, nameList, motivation);
-//     }
-//   }
-// }
-
-// getCategories();
